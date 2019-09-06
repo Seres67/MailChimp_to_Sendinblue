0a50305995c223574aac39e1ddc42a99f9c3bbe6
Laurent Haar <laurent.haar@epitech.eu>
2019-09-06 11:10:12 +0200

    renamed main.h to main.hpp

---
+++ main.hpp
@@ -0,0 +1,9 @@
+#ifndef MAILCHIMP_TO_SENDINBLUE_MAIN_HPP
+#define MAILCHIMP_TO_SENDINBLUE_MAIN_HPP
+
+typedef struct convert {
+    const char *base;
+    const char *convert;
+} convert_t;
+
+#endif //MAILCHIMP_TO_SENDINBLUE_MAIN_HPP
