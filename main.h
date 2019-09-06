fc14f418562ac28b75b3ba33b3a894beb7782096
Laurent <laurent.haar@epitech.eu>
2019-09-06 16:00:18 +0200

    what am i doing ???

--- main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAILCHIMP_TO_SENDINBLUE_MAIN_H
-#define MAILCHIMP_TO_SENDINBLUE_MAIN_H
-
-typedef struct convert {
-    const char *base;
-    const char *convert;
-} convert_t;
-
-#endif //MAILCHIMP_TO_SENDINBLUE_MAIN_H
